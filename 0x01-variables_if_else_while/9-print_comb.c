bae1b4843c6b0350c77a10a59feb64ffd3df4967
Haqq Bassit <munagahmnh@gmail.com>
2023-02-18 01:57:35 +0000

    terminate with newline

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,16 @@ int main(void)
 	while (i < 10)
 	{
 		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
-		i++;
+		if (i == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
 	}
 	putchar('\n');
 	return (0);
