4ccd2dd5afa0f65f2cf2e3972de0f573f5145332
Haqq Bassit <munagahmnh@gmail.com>
2023-02-22 00:42:47 +0000

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - fxn to compute absolute value
+ *@n: number
+ *Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
