a1a22ec2e340188e3086d90cbc6725bfee11fa64
Haqq Bassit <munagahmnh@gmail.com>
2023-03-02 14:09:43 +0000

    bug fix for strings with diff lengths

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, string1, string2, comp;
 
-	for (i = string1 = string2 = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = string1 = string2 = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
 			string1 += 1;
