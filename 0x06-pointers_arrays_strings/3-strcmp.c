910c621db4ae4a5ba386f8f5c74368da26c458e4
Haqq Bassit <munagahmnh@gmail.com>
2023-03-02 14:02:20 +0000

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,22 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, comp;
+	int i, string1, string2, comp;
 
-	for (i = comp = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = string1 = string2 = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
-			comp += 1;
+			string1 += 1;
 		else if (s1[i] < s2[i])
-			comp -= 1;
+			string2 += 1;
 	}
+
+	if (string1 > string2)
+		comp = 1;
+	else if (string1 < string2)
+		comp = -1;
+	else
+		comp = 0;
 
 	return (comp);
 }
