eae64b6f8b709d01ee0732e8b57a8d9b06489cc6
Haqq Bassit <munagahmnh@gmail.com>
2023-03-03 01:16:29 +0000

    leetcode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/**
+ *leet - fxn that encodes a string in leetcode
+ *@s: string
+ *Return: pointer to s
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'A' || s[i] == 'a')
+			*(s + i) = '4';
+		else if (s[i] == 'E' || s[i] == 'e')
+			*(s + i) = '3';
+		else if (s[i] == 'O' || s[i] == 'o')
+			*(s + i) = '0';
+		else if (s[i] == 'T' || s[i] == 't')
+			*(s + i) = '7';
+		else if (s[i] == 'L' || s[i] == 'l')
+			*(s + i) = '1';
+	}
+	return (s);
+}
