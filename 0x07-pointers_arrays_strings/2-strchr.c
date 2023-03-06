f9f9546522646ec800be4f20749404b10d77fc68
Haqq Bassit <munagahmnh@gmail.com>
2023-03-06 17:04:53 +0000

    check null byte

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; 1; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
+	if (s[i] == c)
+		return (s + i);
+
 	return ('\0');
 }
