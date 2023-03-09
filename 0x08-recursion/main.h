35265522c271ca8c26a6ca34e17ddf8ece88104e
Haqq Bassit <munagahmnh@gmail.com>
2023-03-09 21:19:59 +0000

    prevent double inclusion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
 int _strlen_recursion(char *);
 int factorial(int);
 int _pow_recursion(int, int);
+int _sqrt(int, int);
 int _sqrt_recursion(int);
+
+#endif
