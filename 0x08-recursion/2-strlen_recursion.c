d11c581ec1704a4602a491b0460423f54f0c7d5a
Haqq Bassit <munagahmnh@gmail.com>
2023-03-09 00:06:01 +0000

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ *_strlen_recursion - function that returns length of string
+ *@s:string
+ *Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
