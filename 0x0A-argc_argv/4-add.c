4dfafa0b07942385586559fca541ab5d99db96f1
Haqq Bassit <munagahmnh@gmail.com>
2023-03-12 11:21:58 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function to add command line arguments
+ *@argc: number of cl args
+ *@argv: command line arguments
+ *Return: 0 if successful and 1 if failed
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
